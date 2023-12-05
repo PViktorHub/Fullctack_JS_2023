fc2205147ac5f4750c18aa0ebc00e3a4969664bb
Viktor.Ponomarenko <pvikmbox@gmail.com>
2023-12-05 21:12:04 +0200

    Home Task 2023-12-05

---
+++ 202312/HomeTask20231205.js
@@ -0,0 +1,80 @@
+
+{
+  // case 1
+  let place = 'Вечірнє кафе в паризькому провулку';
+  let dateTime = new Date(2023, 12, 6, 19, 30, 0);
+  let user1Name = 'Марія';
+  let user2Name = 'П\'єр';
+
+
+  let actionItem = {
+    place:place,
+    dateTime:dateTime,
+    user1Name:user1Name,
+    user2Name:user2Name,
+  };
+
+  console.log(actionItem);
+}
+
+{
+  // case 2
+  let place = 'На виставці сучасного мистецтва у Нью-Йорку';
+  let dateTime = new Date(2023, 12, 6, 15, 00, 0);
+  let user1Name = 'Майк';
+  let user2Name = 'Амелією';
+  let user3Name = 'Саймон';
+
+
+  let actionItem = {
+    place:place,
+    dateTime:dateTime,
+    user1Name:user1Name,
+    user2Name:user2Name,
+    user3Name:user3Name
+  };
+
+  console.log(actionItem);
+}
+
+{
+  // case 3
+  let place = 'На пляжі у Гоа ';
+  let dateTime = new Date(2023, 12, 6, 11, 00, 0);
+  let user1Name = 'Кейт';
+  let user2Name = 'Джеймс';
+  let user3Name = 'Радж';
+  let user4Name = 'Лео';
+
+
+  let actionItem = {
+    place:place,
+    dateTime:dateTime,
+    user1Name:user1Name,
+    user2Name:user2Name,
+    user3Name:user3Name,
+    user4Name:user4Name
+  };
+
+  console.log(actionItem);
+}
+
+{
+  // case 4
+  let place = 'На вечірці в сучасному музеї у Токіо';
+  let dateTime = new Date(2023, 12, 6, 21, 00, 0);
+  let user1Name = 'Юка';
+  let user2Name = 'Дайго';
+  let user3Name = 'Міюко';
+
+
+  let actionItem = {
+    place:place,
+    dateTime:dateTime,
+    user1Name:user1Name,
+    user2Name:user2Name,
+    user3Name:user3Name,
+  };
+
+  console.log(actionItem);
+}
